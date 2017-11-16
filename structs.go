15eb3c389f72652da4a0e09107af93f731beb287
astrothing <andres.rodriguezhn@gmail.com>
2017-11-16 10:13:12 -0600

    Added FloatTableColumn

--- structs.go
+++ structs.go
@@ -1,15 +1,17 @@
+package common
+
 type comms struct {
-	Type int 	`json:"Type"`
-	Data string	`json:"Data"`
+	Type int    `json:"Type"`
+	Data string `json:"Data"`
 }
 
 type database struct {
-	DB_Name string 	`json:"DB_Name"`
-	Tables table	`json:"Tables"`
+	DB_Name string `json:"DB_Name"`
+	Tables  table  `json:"Tables"`
 }
 
 type table struct {
-	Table_Name string		`json:"Table_Name"`
-	ColumnNames []string	`json:"ColumnNames"`
-	ColumnTypes []string	`json:"ColumnTypes"`
+	Table_Name  string   `json:"Table_Name"`
+	ColumnNames []string `json:"ColumnNames"`
+	ColumnTypes []string `json:"ColumnTypes"`
 }
